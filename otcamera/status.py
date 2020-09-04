7e94adfa9d898e29614e7f2cf3e1e1f15e2c9444
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2020-09-04 18:51:13 +0200

    added status informations

---
+++ otcamera/status.py
@@ -0,0 +1,38 @@
+# OTCamera status variables
+# Copyright (C) 2020 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam
+# <team@opentrafficcam.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import datetime as dt
+import config
+
+shutdownactive = False
+noblink = False
+wifiapon = False
+
+
+def record_time():
+    current_hour = dt.datetime.now().hour
+    record_time = (
+        # TODO: #10 reference hardware config
+        (hour_switch.is_pressed)
+        or (current_hour >= config.STARTHOUR and current_hour < config.ENDHOUR)
+    ) and (not shutdownactive)
+    return record_time
+
+
+def preview_on():
+    return wifiapon
