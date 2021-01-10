8b69c110221d8085eb41e93bd725bc89667cdcbc
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2021-01-10 23:30:35 +0100

    comments

--- OTCamera/config.py
+++ OTCamera/config.py
@@ -19,22 +19,30 @@
 
 import socket
 
+# Turn debug mode on to get addition log entries
 DEBUG = False
 
+# Hour of day when to start/end recording
 STARTHOUR = 6
 ENDHOUR = 22
+# Interval length in minutes before video splits
+INTERVAL = 15
+# Number of intervals to record (0=infinit)
+N_INTERVALS = 2
+# Interval between two preview images in seconds
+PREVIEW_INTERVAL = 5
 
-HOSTNAME = socket.gethostname()
-
-# file settings
-
+# free space on sd card before old videos get deleted
 MINFREESPACE = 1  # GB
+# prefix for videoname and annotation
+PREFIX = socket.gethostname()
+# path to safe videofiles
 VIDEOPATH = "/home/pi/videos/"
+# path to save preview
 PREVIEWPATH = "/home/pi/preview.jpg"
 
 
 # camera settings
-
 FPS = 20
 RESOLUTION = (1640, 1232)
 EXPOSURE_MODE = "nightpreview"
@@ -42,10 +50,8 @@ DRC_STRENGTH = "high"
 ROTATION = 180
 
 # video settings
-
 FORMAT = "h264"
 PREVIEWFORMAT = "jpeg"
-INTERVAL = 15
 RESIZE = (800, 600)
 PROFILE = "high"
 LEVEL = "4"
@@ -53,7 +59,6 @@ BITRATE = 600000
 QUALITY = 30
 
 # hardware settings
-
 USE_LED = False
 USE_BUTTONS = False
 WIFIDELAY = 900
