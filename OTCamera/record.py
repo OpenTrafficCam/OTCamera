51efba4bbd4b1787a4a6d5a3c4bbb6a67e1bdca5
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2021-01-10 23:48:33 +0100

    main record function

---
+++ OTCamera/record.py
@@ -0,0 +1,56 @@
+# OTVision: record a video for specific time period
+# Copyright (C) 2020 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam
+# <team@opentrafficcam.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import config
+from hardware import camera, leds
+from helpers import log
+from status import record_time
+import status
+
+
+def init():
+    log.write("#")
+    log.write("starting periodic record")
+    leds.power_on()
+    # TODO: turn wifi AP on
+
+
+def loop():
+    try:
+        if record_time():
+            camera.start_recording()
+            camera.split_if_interval_ends()
+        else:
+            camera.stop_recording()
+            camera.wait_recording(0.5)
+    except (KeyboardInterrupt):
+        log.write("Keyboard Interrupt, Stopping", level="warning")
+        return "break"
+
+
+def record():
+    init()
+    while True:
+        if config.N_INTERVALS == 0:
+            pass
+        elif status.current_interval == config.N_INTERVALS:
+            break
+        loop()
+    camera.stop_recording()
+    log.closefile()
