35396f6f232708f55339e61eaed0ecaf3b07308c
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2021-01-23 01:03:08 +0100

    config

--- OTCamera/config.py
+++ OTCamera/config.py
@@ -1,26 +1,28 @@
-# OTCamera: static config file to be imported.
-# Copyright (C) 2020 OpenTrafficCam Contributors
-# <https://github.com/OpenTrafficCam
+"""OTCamera config variables.
+
+All the configuration of OTCamera is done here.
+
+"""
+# Copyright (C) 2021 OpenTrafficCam Contributors
+# <https://github.com/OpenTrafficCam>
 # <team@opentrafficcam.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import socket
 
 # Turn debug mode on to get addition log entries
-DEBUG = True
+DEBUG = False
 
 # Hour of day when to start/end recording
 STARTHOUR = 6
