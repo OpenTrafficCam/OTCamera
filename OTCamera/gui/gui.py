05bc2faf85ab8334bbf4cae5d48adf70add15511
hgoerner <hendrikg.goerner@tu-dresden>
2022-04-11 10:49:21 +0200

    disabled gui

--- OTCamera/gui/gui.py
+++ OTCamera/gui/gui.py
@@ -1,37 +1,37 @@
-import config, status
-import PySimpleGUIWeb as sg
-from time import sleep
+# 'import config, status
+# import PySimpleGUIWeb as sg
+# from time import sleep
 
 
-def main():
-    #camera.preview(now=True)
+# def main():
+#     #camera.preview(now=True)
 
 
-    layout = [
-        [sg.Text("OpenTrafficCam OTCamera")],
-        [sg.Image(filename=None, key="preview")],
-        [sg.Button("New Preview", key="previewbutton")],
-    ]
+#     layout = [
+#         [sg.Text("OpenTrafficCam OTCamera")],
+#         [sg.Image(filename=None, key="preview")],
+#         [sg.Button("New Preview", key="previewbutton")],
+#     ]
 
-    window = sg.Window(
-        "OpenTrafficCam",
-        layout,
-        element_padding=(5, 5),
-        web_port=2222,
-        web_start_browser=True,
-        disable_close=True,
-        auto_size_buttons=False,
-    )
-    i = 0
-    while True:
-        event, values = window.read()
-        if event == "previewbutton":
-              print("tada")
-              window["preview"].update(filename=config.PREVIEWPATH)
-        #window["preview"].update(filename=config.PREVIEWPATH)
-        if event is None:
-            break
-        i += 1
-    window.close()
-    print("Program terminating normally")
-
+#     window = sg.Window(
+#         "OpenTrafficCam",
+#         layout,
+#         element_padding=(5, 5),
+#         web_port=2222,
+#         web_start_browser=True,
+#         disable_close=True,
+#         auto_size_buttons=False,
+#     )
+#     i = 0
+#     while True:
+#         event, values = window.read()
+#         if event == "previewbutton":
+#               print("tada")
+#               window["preview"].update(filename=config.PREVIEWPATH)
+#         #window["preview"].update(filename=config.PREVIEWPATH)
+#         if event is None:
+#             break
+#         i += 1
+#     window.close()
+#     print("Program terminating normally")
+'
