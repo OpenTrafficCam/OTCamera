c06a3d369ad43c3a19d212d60d362592db427ad1
hgoerner <75123353+hgoerner@users.noreply.github.com>
2022-04-12 14:42:39 +0200

    Update first_use.sh
    updated shell skript for easy and fast configuration

--- first_use.sh
+++ first_use.sh
@@ -1,14 +1,43 @@
 #! /usr/bin/bash
 
-echo Hello World
-
-read -p "Enter a name for the accesspoint: " HOTSPOTNAME
-read -p "Enter a new passphrase for the accesspoint: " PASSPHRASE
-read -p "Enter a new IP-address for the accespoint (like 10.0.0.5) :" IPADRESS
-
-sed -i 's/^ssid=.*/ssid='$HOTSPOTNAME'/g' /etc/hostapd/hostapd.conf
-sed -i 's/wpa_passphrase=.*/wpa_passphrase='$PASSPHRASE'/g' /etc/hostapd/hostapd.conf
-sed -i 's/    ip a add [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/    ip a add '$IPADRESS'/g' /usr/bin/autohotspot    
-sed -i 's/        <title>\w*/        <title>'$HOTSPOTNAME'/g' /home/admin/OTCamera/webfiles/index.html    
-sed -i 's/        <h1>\w*/        <h1>'$HOTSPOTNAME'/g' /home/admin/OTCamera/webfiles/index.html  
-
+read -p "Change accespointname? Y/N " ANSWER
+case "$ANSWER" in 
+        [yY] | [yY][eE][sS])
+        read -p "Enter a name for the accesspoint: " HOTSPOTNAME
+        sed -i 's/^ssid=.*/ssid='$HOTSPOTNAME'/g' /etc/hostapd/hostapd.conf
+        sed -i 's/        <title>\w*/        <title>'$HOTSPOTNAME'/g' /home/pi/OTCamera/webfiles/index.html    
+        sed -i 's/        <h1>\w*/        <h1>'$HOTSPOTNAME'/g' /home/pi/OTCamera/webfiles/index.html  
+        ;;
+        [nN] | [oO]) 
+        ;;
+esac
+read -p "Change accespointpasswort? Y/N " ANSWER
+case "$ANSWER" in 
+        [yY] | [yY][eE][sS])
+        read -p "Enter a name for the accesspointpassword: " PASSPHRASE
+        sed -i 's/wpa_passphrase=.*/wpa_passphrase='$PASSPHRASE'/g' /etc/hostapd/hostapd.conf
+        ;;
+        [nN] | [oO]) 
+        ;;
+esac
+read -p "Change IP of the accesspoint? Y/N " ANSWER
+case "$ANSWER" in 
+        [yY] | [yY][eE][sS])
+        read -p "Enter a new IP-address for the accespoint (like 10.0.0.5) :" IPADRESS
+        sed -i 's/    ip a add [0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/    ip a add '$IPADRESS'/g' /usr/bin/autohotspot   
+        ;;
+        [nN] | [oO]) 
+        ;;
+esac 
+read -p "Change hostname of raspberry? Y/N " ANSWER
+case "$ANSWER" in 
+        [yY] | [yY][eE][sS])
+        read -p "Enter a new hostname (annotation of video/access in wlan-mode) :" NEWHOSTNAME
+        OLDHOSTNAME=$(hostname)
+        sed -i 's/'$OLDHOSTNAME'/'$NEWHOSTNAME'/g' /etc/hosts
+        hostname -b $NEWHOSTNAME
+        sed -i 's/.*/'$NEWHOSTNAME'/g' /etc/hostname    
+        ;;
+        [nN] | [oO]) 
+        ;;
+esac 
