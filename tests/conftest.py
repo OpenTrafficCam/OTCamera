d28a17c31780baa1da3a74cd79ccf339be91f639
Randy Seng <rseng@pm.me>
2022-05-02 21:24:18 +0200

    Add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import shutil
+
+from pathlib import Path
+import pytest
+
+
+@pytest.fixture
+def test_dir() -> Path:
+    test_dir = Path(__file__).parent / "data"
+    test_dir.mkdir(exist_ok=True)
+    yield test_dir
+    shutil.rmtree(test_dir)
