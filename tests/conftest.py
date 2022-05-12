94df30723832f8b19eb0a3e605cfc9ab75ef2e98
Randy Seng <rseng@pm.me>
2022-05-12 19:00:40 +0200

    Clean up directory after every test run

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 import pytest
@@ -8,3 +9,8 @@ def test_dir() -> Path:
     test_dir = Path(__file__).parent / "data"
     test_dir.mkdir(exist_ok=True)
     yield test_dir
+    for f in test_dir.iterdir():
+        try:
+            f.unlink()
+        except FileNotFoundError:
+            shutil.rmtree(f)
