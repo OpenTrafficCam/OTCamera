f5fde603cb8e1ea701ff7faad8fa45037a1196a9
Randy Seng <rseng@pm.me>
2022-05-19 18:45:15 +0200

    Fix test

--- tests/helpers/name_test.py
+++ tests/helpers/name_test.py
@@ -10,7 +10,7 @@ def test_log_correctFilename(mock_current_dt: mock.MagicMock):
     actual = name.log()
     expected = f"{config.VIDEO_DIR}/{config.PREFIX}_2022-05-18_22-00-59.log"
     mock_current_dt.assert_called_once()
-    assert actual == expected
+    assert str(actual) == expected
 
 
 @mock.patch("OTCamera.helpers.name._current_dt", return_value="2022-05-18_22-00-59")
