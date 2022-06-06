12580c5f83afc92bc05bee9d164b8bb67caef066
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2022-06-06 20:35:19 +0200

    added ssh relay install and service script

---
+++ raspi-files/install_sshrelay.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "OTCamera SSH relay server setup"
+echo "############################"
+echo " "
+
+read -r -e -p "server address: " -i "relay.opentrafficcam.org" SERVER
+read -r -e -p "server port: " -i "22" PORT
+read -r -e -p "public key: " PUBKEY
+
+
+echo "### Generating new SSH key"
+runuser -l "$SUDO_USER" -c "ssh-keygen -t ed25519 -q -f '/home/$SUDO_USER/.ssh/id_ed25519' -N ''"
+
+echo "### Adding remote server public key as authorized key"
+HOME="/home/$SUDO_USER"
+echo "" >> "$HOME"/.ssh/authorized_keys
+echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys
+
+echo ""
+echo ""
+cat "/home/$SUDO_USER/.ssh/id_ed25519.pub"
+echo ""
+echo ""
+
+read -r -e -p "Copy public key above to relay server (username: $HOSTNAME)"
+
+echo "### Configuring service"
+cd "$HOME/OTCamera" || { echo "Error: Cannot find OTCamera directory"; exit 1; }
+RELAYSERVICE="./raspi-files/sshrelay.service"
+cp $RELAYSERVICE /lib/systemd/system
+RELAYSERVICE="/lib/systemd/system/sshrelay.service"
+
+SSHCMD="/bin/ssh -NT -R 0:localhost:22 $HOSTNAME@$SERVER -p $PORT"
+
+sed $RELAYSERVICE -i -e "s?^User=username?User=$SUDO_USER?g"
+sed $RELAYSERVICE -i -e "s?^ExecStart=/bin/ssh -NT -R 0:localhost:22 hostname@server -p port?ExecStart=$SSHCMD?g"
+
+echo "    ServerAliveInterval 240" >> /etc/ssh/ssh_config
+
+systemctl daemon-reload
+systemctl disable sshrelay.service
+
+echo "### please connect once to add host key using:"
+echo "$SSHCMD"
+
+
+echo "#########"
+echo "Done."
+echo "#########"
