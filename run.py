db3b5693bade1fe155e98d0c2ee6988c9c481050
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2022-09-20 16:30:05 +0200

    Make it possible to pass path to custom config file over the command line

--- run.py
+++ run.py
@@ -1,4 +1,38 @@
-from OTCamera import record
+import argparse
+import sys
+from pathlib import Path
+
+import OTCamera.config as config
+
+
+def parse_args() -> Path:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        help="the absolute path to your custom config file.",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    if args.config is not None and not Path(args.config).exists():
+        raise FileNotFoundError(f"The user config '{args.config}' does not exist.")
+
+    if args.config:
+        config.parse_user_config(args.config)
+    else:
+        config.parse_user_config("./user_config.yaml")
+
+
+def main():
+    parse_args()
+
+    import OTCamera.record as record
+
+    record.main()
+
 
 if __name__ == "__main__":
-    record.main()
+    module = sys.modules[__name__]
+    main()
