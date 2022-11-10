5ce7237dc6d45303daf5e3183304b06822265ecc
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2022-11-10 16:38:41 +0100

    Add static methods to top to avoid NameError when calling functions within the module

--- OTCamera/config.py
+++ OTCamera/config.py
@@ -30,101 +30,6 @@ except ImportError:
 
 import yaml
 
-# general config
-DEBUG_MODE_ON = False
-"""Turn debug mode on to get additional log entries."""
-USE_RELAY = False
-"""Enable to start and stop sshrelay.service (need's to be configured)"""
-
-# recording config
-START_HOUR = 6
-"""Hour of day when to start recording."""
-END_HOUR = 22
-"""Hour of day when to end recording."""
-INTERVAL_LENGTH = 15
-"""Interval length in minutes before video splits."""
-NUM_INTERVALS = 0
-"""Number of full intervals to record (0=infinit)."""
-MIN_FREE_SPACE = 1
-"""free space in GB on sd card before old videos get deleted."""
-
-# camera config
-FPS = 20
-"""Frames per Second. 10-20 should be enough."""
-RESOLUTION = (1640, 1232)
-"""Resolution of the camera module works internally.
-Field of view could be smaller with other values."""
-EXPOSURE_MODE = "nightpreview"
-"""Controls the analog and digital gains."""
-DRC_STRENGTH = "high"
-"""Sets dynamic range compression to lighten dark areas and to darken light areas."""
-ROTATION = 180
-"""Rotate the whole camera image."""
-AWB_MODE = "greyworld"
-"""Controls the auto white balancing mode. `greyworld`
-is a specific mode for NoIR modules."""
-METER_MODE = "average"
-"""Controls the size of the center region to adjust exposure."""
-
-# preview settings
-PREVIEW_PATH = "~/OTCamera/webfiles/preview.jpg"
-"""path to save preview."""
-PREVIEW_FORMAT = "jpeg"
-"""Filetype of the static preview image."""
-PREVIEW_INTERVAL = 5
-"""Interval between two preview images in seconds."""
-
-# video config
-VIDEO_DIR = "~/videos/"
-"""path to safe videofiles."""
-VIDEO_FORMAT = "h264"
-"""Encoding format."""
-RESOLUTION_SAVED_VIDEO_FILE = (800, 600)
-"""Resolution of the saved videofile, not the camera itself."""
-H264_PROFILE = "high"
-"""Profile used in h264 encoder."""
-H264_LEVEL = "4"
-"""Level used in h264 encoder."""
-H264_BITRATE = 600000
-"""Bitrate used in h264 encoder."""
-H264_QUALITY = 30
-"""Quality used in h264 encoder."""
-
-# Wi-Fi config
-WIFI_DELAY = 900
-"""Delay in seconds before wifi turns off."""
-
-# LED config
-USE_LED = False
-"""True if Status-LEDs are connected."""
-
-# button config
-USE_BUTTONS = False
-"""True if hardware buttons are connected."""
-
-# other config
-PREFIX = socket.gethostname()
-"""prefix for videoname and annotation."""
-TEMPLATE_HTML_PATH = "~/OTCamera/webfiles/template.html"
-"""Path to template HTML"""
-INDEX_HTML_PATH = "~/OTCamera/webfiles/index.html"
-"""Path to the auto generated index HTML."""
-OFFLINE_HTML_PATH = "~/OTCamera/webfiles/offline.html"
-"""Path to the HTML to be displayed when OTCamera is offline"""
-NUM_LOG_FILES_HTML = 2
-"""Number of log files to be displayed on the status website"""
-
-# Microsoft Teams WebHook
-USE_MS_TEAMS_WEBHOOK = False
-MS_TEAMS_WEBHOOK_URL = None
-MS_TEAMS_MAX_FAILED_SEND_ATTEMPTS = 2
-
-VIDEO_DIR = str(Path(VIDEO_DIR).expanduser().resolve())
-PREVIEW_PATH = str(Path(PREVIEW_PATH).expanduser().resolve())
-TEMPLATE_HTML_PATH = str(Path(TEMPLATE_HTML_PATH).expanduser().resolve())
-INDEX_HTML_PATH = str(Path(INDEX_HTML_PATH).expanduser().resolve())
-OFFLINE_HTML_PATH = str(Path(OFFLINE_HTML_PATH).expanduser().resolve())
-
 
 def parse_user_config(config_file: str):
     config_file = str(Path(config_file).expanduser().resolve())
@@ -338,3 +243,99 @@ def parse_user_config(config_file: str):
 
 def _print_key_err_msg(key_name: str) -> None:
     print(f"KeyError in config file for: '{key_name}'")
+
+
+# general config
+DEBUG_MODE_ON = False
+"""Turn debug mode on to get additional log entries."""
+USE_RELAY = False
+"""Enable to start and stop sshrelay.service (need's to be configured)"""
+
+# recording config
+START_HOUR = 6
+"""Hour of day when to start recording."""
+END_HOUR = 22
+"""Hour of day when to end recording."""
+INTERVAL_LENGTH = 15
+"""Interval length in minutes before video splits."""
+NUM_INTERVALS = 0
+"""Number of full intervals to record (0=infinit)."""
+MIN_FREE_SPACE = 1
+"""free space in GB on sd card before old videos get deleted."""
+
+# camera config
+FPS = 20
+"""Frames per Second. 10-20 should be enough."""
+RESOLUTION = (1640, 1232)
+"""Resolution of the camera module works internally.
+Field of view could be smaller with other values."""
+EXPOSURE_MODE = "nightpreview"
+"""Controls the analog and digital gains."""
+DRC_STRENGTH = "high"
+"""Sets dynamic range compression to lighten dark areas and to darken light areas."""
+ROTATION = 180
+"""Rotate the whole camera image."""
+AWB_MODE = "greyworld"
+"""Controls the auto white balancing mode. `greyworld`
+is a specific mode for NoIR modules."""
+METER_MODE = "average"
+"""Controls the size of the center region to adjust exposure."""
+
+# preview settings
+PREVIEW_PATH = "~/OTCamera/webfiles/preview.jpg"
+"""path to save preview."""
+PREVIEW_FORMAT = "jpeg"
+"""Filetype of the static preview image."""
+PREVIEW_INTERVAL = 5
+"""Interval between two preview images in seconds."""
+
+# video config
+VIDEO_DIR = "~/videos/"
+"""path to safe videofiles."""
+VIDEO_FORMAT = "h264"
+"""Encoding format."""
+RESOLUTION_SAVED_VIDEO_FILE = (800, 600)
+"""Resolution of the saved videofile, not the camera itself."""
+H264_PROFILE = "high"
+"""Profile used in h264 encoder."""
+H264_LEVEL = "4"
+"""Level used in h264 encoder."""
+H264_BITRATE = 600000
+"""Bitrate used in h264 encoder."""
+H264_QUALITY = 30
+"""Quality used in h264 encoder."""
+
+# Wi-Fi config
+WIFI_DELAY = 900
+"""Delay in seconds before wifi turns off."""
+
+# LED config
+USE_LED = False
+"""True if Status-LEDs are connected."""
+
+# button config
+USE_BUTTONS = False
+"""True if hardware buttons are connected."""
+
+# other config
+PREFIX = socket.gethostname()
+"""prefix for videoname and annotation."""
+TEMPLATE_HTML_PATH = "~/OTCamera/webfiles/template.html"
+"""Path to template HTML"""
+INDEX_HTML_PATH = "~/OTCamera/webfiles/index.html"
+"""Path to the auto generated index HTML."""
+OFFLINE_HTML_PATH = "~/OTCamera/webfiles/offline.html"
+"""Path to the HTML to be displayed when OTCamera is offline"""
+NUM_LOG_FILES_HTML = 2
+"""Number of log files to be displayed on the status website"""
+
+# Microsoft Teams WebHook
+USE_MS_TEAMS_WEBHOOK = False
+MS_TEAMS_WEBHOOK_URL = None
+MS_TEAMS_MAX_FAILED_SEND_ATTEMPTS = 2
+
+VIDEO_DIR = str(Path(VIDEO_DIR).expanduser().resolve())
+PREVIEW_PATH = str(Path(PREVIEW_PATH).expanduser().resolve())
+TEMPLATE_HTML_PATH = str(Path(TEMPLATE_HTML_PATH).expanduser().resolve())
+INDEX_HTML_PATH = str(Path(INDEX_HTML_PATH).expanduser().resolve())
+OFFLINE_HTML_PATH = str(Path(OFFLINE_HTML_PATH).expanduser().resolve())
