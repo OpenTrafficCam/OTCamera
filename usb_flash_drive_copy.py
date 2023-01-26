64379d28531a2b7529449cf8ff69b29a3529c8f1
Randy Seng <19281702+randy-seng@users.noreply.github.com>
2023-01-26 16:52:12 +0100

    WIP

--- usb_flash_drive_copy.py
+++ usb_flash_drive_copy.py
@@ -1,12 +1,14 @@
-from dataclasses import dataclass
-from pathlib import Path
 import csv
-import shutil
-import socket
-import re
-from gpiozero import PWMLED
 import logging
 import os
+import re
+import shutil
+import socket
+import subprocess
+from dataclasses import dataclass
+from pathlib import Path
+
+from gpiozero import PWMLED
 
 
 class IsNotADirectoryError(OSError):
@@ -27,6 +29,9 @@ class Video:
         delete = bool(re.search(r"yes|y|true|x|ja|j", d["delete"]))
         return Video(filename, src / filename, copied, delete)
 
+    def to_dict(self) -> dict:
+        return {"filename": self.filename, "copied": self.copied, "delete": self.delete}
+
 
 class Led:
     def __init__(self, led: PWMLED) -> None:
@@ -45,6 +50,7 @@ class Led:
 @dataclass
 class CopyInformation:
     videos: list[Video]
+    csv_field_names: str
     csv_file: Path
     src_dir: Path
     dest_dir: Path
@@ -59,8 +65,12 @@ class CopyInformation:
 
         return CopyInformation(videos, file, src_dir, dest_dir)
 
-    def to_dict(self) -> dict:
-        pass
+    def to_dict(self) -> list[dict]:
+        new_videos: list[Video] = []
+        for video in self.videos:
+            video_dict = video.to_dict()
+            new_videos.append(video_dict)
+        return new_videos
 
 
 class OTCameraUsbCopier:
@@ -69,13 +79,12 @@ class OTCameraUsbCopier:
         red_led: Led,
         green_led: Led,
         src_dir: Path,
-        dest_dir: Path,
+        usb_mount: Path,
     ) -> None:
         self.red_led = red_led
         self.green_led = green_led
         self.src_dir = src_dir
-        self.dest_dir = dest_dir
-        logging.basicConfig(filename=src_dir / "log", encoding="utf-8")
+        self.usb_mount = usb_mount
 
     def copy_over_usb(self, copy_info: CopyInformation) -> None:
         self.red_led.blink()
@@ -90,7 +99,7 @@ class OTCameraUsbCopier:
 
             shutil.copy2(
                 src=video.path,
-                dst=self.dest_dir / video.filename,
+                dst=self.usb_mount / video.filename,
             )
             video.copied = True
             logging.info(f"Video: '{video.path}' copied.")
@@ -109,13 +118,26 @@ class OTCameraUsbCopier:
             video.path.unlink()
             logging.info(f"Video at: '{video.path}' deleted!")
 
-    def update(self) -> None:
-        pass
+    def update(self, copy_info: CopyInformation) -> None:
+        with open(copy_info.csv_file, "w", newline="") as csv_file:
+            writer = csv.DictWriter(
+                csv_file, fieldnames=["filename", "copied", "delete"]
+            )
+            writer.writeheader()
+            for video_info in copy_info.to_dict():
+                writer.writerow(video_info)
+
+    def unmount_usb(self) -> None:
+        completedProcess: subprocess.CompletedProcess = subprocess.run(
+            f"umount {self.usb_mount}"
+        )
+        if completedProcess.returncode != 0:
+            logging.error(f"Unable to unmount '{self.usb_mount}'!")
 
 
 def get_video_files(dirpath: Path, filetype: str) -> list[Path]:
     if not dirpath.is_dir():
-        raise IsNotADirectoryError(f"Path: '{dirpath} is not a directory!'")
+        raise IsNotADirectoryError(f"Path: '{dirpath}' is not a directory!")
     return [f for f in dirpath.iterdir() if f.is_file() and f.suffix == f".{filetype}"]
 
 
@@ -126,6 +148,7 @@ def get_hostname() -> str:
 def main():
     src_dir = Path(f"/home/{os.getlogin()}/videos")
     dest_dir = Path(__file__).parent / "tests/data"
+    logging.basicConfig(filename=src_dir / "log", encoding="utf-8")
     usb_copy_info_path = (
         Path(__file__).parent / "tests/data/otcamera-dev01_usb-copy-info.csv"
     )
