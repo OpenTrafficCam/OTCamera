14562de25d9efa061203500d285890dad191b8e8
David Helbig <david.helbig@platomo.de>
2026-06-01 15:39:51 +0200

    fix documentation

--- OTCamera/netwatch/probe.py
+++ OTCamera/netwatch/probe.py
@@ -52,7 +52,7 @@ class HttpNetworkProbe(NetworkProbe):
 
         Sends http requests to the urls defined in `HttpNetworkProbe.urls` in sequnce.
 
-        If both requests result in a `RequestException`, the network status is
+        If all requests result in a `RequestException`, the network status is
         considered OFFLINE.
 
         Returns:
